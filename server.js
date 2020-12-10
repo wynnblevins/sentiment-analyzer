e5638eee7930b6bd1a940e53d79da8b31930f195
Wynn <wynn.blevins@apivista.com>
2020-12-10 09:51:51 -0500

    Removing express related code in favor of kafka.

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
 const Sentiment = require('sentiment');
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const app = express();
-const PORT = process.env.PORT || 8080;
-const path = require('path');
 const commons = require('@market-predictor/market-predictor-commons');
 
 require('dotenv').config()
-
-// ===== Middleware ====
-app.use(morgan('dev'))
-app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
-);
-app.use(bodyParser.json())
-
-// ====== Error handler ====
-app.use(function(err, req, res, next) {
-	console.log('====== ERROR =======')
-	console.error(err.stack)
-	res.status(500)
-});
 
 const sentimentService = require('./services/sentiment.service')(Sentiment);
 
@@ -38,8 +16,14 @@ var kafka = require('kafka-node'),
         {
             autoCommit: false
         }
-    );
+	);
+	
+consumer.on('error', (err) => {
+	console.log(err);
+	console.log('error encountered');
+});	
 consumer.on('message', function (message) {
+	console.log('message received');
 	const sentimentResult = sentimentService.getSentiment(message);
 	
 	commons.SentimentRecord.create({ 
@@ -47,14 +31,4 @@ consumer.on('message', function (message) {
 		text: sentimentResult.text,
 		score: sentimentResult.sentiment_score 
 	})
-});
-
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.resolve('build/')));
-}
-
-// ==== Starting Server =====
-app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`)
 });
